e26ece5319539804a9ee76336d8978fd6d42e9e9
Casey Primozic <me@ameo.link>
2024-10-05 22:51:30 -0700

    Update Sentry DSN

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,17 +1,11 @@
 import { browser } from '$app/environment';
 import * as Sentry from '@sentry/browser';
-import { Integrations } from '@sentry/tracing';
 
 const sentryEnabled = () => browser && !window.location.href.includes('localhost');
 
 export const maybeInitSentry = () => {
   if (sentryEnabled()) {
-    Sentry.init({
-      dsn: 'https://ed700210c7d54db68c4715c48de71f7a@sentry.ameo.design/13',
-      integrations: [new Integrations.BrowserTracing()],
-
-      tracesSampleRate: 1.0,
-    });
+    Sentry.init({ dsn: 'https://ed700210c7d54db68c4715c48de71f7a@sentry.ameo.design/13' });
   }
 };
 
